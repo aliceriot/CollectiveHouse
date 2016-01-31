e567b6a32a519e3366463dea17ff59857bbeeb19
Alice Pote <alice.writes.wrongs@gmail.com>
2016-01-31 13:52:39 -0500

    Revert "removed all the react-webpack-rails stufffff that we dun want"
    This reverts commit 30a0392135b95477dab838b80c9ac16d7420b78e.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,5 @@
+//= require react_integration
+//= require react_bundle
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
---
+++ app/react/index.js
@@ -0,0 +1,5 @@
+import RWR from 'react-webpack-rails';
+window.RWR = RWR;
+
+import HelloWorld from './components/hello-world';
+RWR.registerComponent('HelloWorld', HelloWorld);
