4e1d578d9d08c3a825cedd2814ca70c6b5f8d08c
Alice Pote <alice.writes.wrongs@gmail.com>
2016-03-03 07:24:59 -0500

    set the host for the dev server (so it works in vagrant)

--- webpack/dev.config.js
+++ webpack/dev.config.js
@@ -3,5 +3,8 @@ const config = require('./../webpack.config');
 
 config.plugins.push(new WebpackNotifierPlugin());
 config.devtool = 'eval-source-map';
+config.devServer = {
+  host: '0.0.0.0'
+};
 
 module.exports = config;
