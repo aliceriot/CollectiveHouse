d2cbcdac9d631542d2909c6ea4b9f92e961a38fc
Alice Pote <alice.writes.wrongs@gmail.com>
2016-03-03 07:44:53 -0500

    adjust the example, move react stuff below turbolinks closes #7

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,9 +1,9 @@
-//= require react_integration
-//= require react_bundle
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
 //= require bootstrap-sprockets
 //= require moment
 //= require fullcalendar
+//= require react_integration
+//= require react_bundle
 //= require_tree .
