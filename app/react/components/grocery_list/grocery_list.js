111ce46dc87171662ba88fbe6d059c84e0d5605a
Alice Pote <alice.writes.wrongs@gmail.com>
2016-03-03 17:40:52 -0500

    initial grocery list component closes #5

---
+++ app/react/components/grocery_list/grocery_list.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { render } from "react-dom";
+
+class GroceryList extends React.Component {
+  render () {
+    return (
+      <div>Hello, world!</div>
+    );
+  }
+}
+
+export default GroceryList;
