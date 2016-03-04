3afb7ee9e01b442b1a0ad3d72d529fa8f95cddb4
Alice Pote <alice.writes.wrongs@gmail.com>
2016-03-04 17:30:55 -0500

    starting to set up the redux stuff

--- app/react/components/grocery_list/grocery_list.js
+++ app/react/components/grocery_list/grocery_list.js
@@ -1,9 +1,13 @@
 import React from "react";
 import { render } from "react-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import GroceryListContents from "./grocery_list_contents";
 
 class GroceryList extends React.Component {
   render () {
     return (
+      <Provider store = {
       <div>Hello, world!</div>
     );
   }
---
+++ app/react/components/grocery_list/grocery_list_contents.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { render } from "react-dom";
+
+class GroceryListContents extends React.Component {
+  render () {
+    return (
+      <div>Hello, world!</div>
+    );
+  }
+}
+
+export default GroceryListContents;
