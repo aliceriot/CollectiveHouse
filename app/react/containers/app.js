25e71d3bceb8675cf559231e5e8ba79e9327164c
Alice Pote <alice.writes.wrongs@gmail.com>
2016-04-16 10:45:31 -0400

    super basic skeleton of react-router setup

--- app/react/containers/app.js
+++ app/react/containers/app.js
@@ -1,11 +1,58 @@
 import React from 'react';
+import { Router, Link, Route, browserHistory } from 'react-router';
+
+class Root extends React.Component {
+  render () {
+    return(
+      <div>
+        root
+        <br/>
+        <Link to="hey">hey</Link>
+        <br/>
+        <Link to="you">hey</Link>
+      </div>
+    );
+  }
+}
+
+
+class Hey extends React.Component {
+  render () {
+    return(
+      <div>
+        hey
+        <br/>
+        <Link to="hey">hey</Link>
+        <br/>
+        <Link to="you">you</Link>
+      </div>
+    );
+  }
+}
+
+class You extends React.Component {
+  render () {
+    return(
+      <div>
+        you
+        <br/>
+        <Link to="hey">hey</Link>
+        <br/>
+        <Link to="you">hey</Link>
+      </div>
+    );
+  }
+}
 
 class App extends React.Component {
   render () {
     return (
-      <div id="foo">
-        hey
-      </div>
+      <Router history={browserHistory}>
+        <Route path="/" component={Root}>
+          <Route path="hey" component={Hey} />
+          <Route path="you" component={You} />
+        </Route>
+      </Router>
     );
   }
 }
