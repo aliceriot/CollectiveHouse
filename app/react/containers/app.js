e7b9e3bb43e2e376c0eaffbc9257154190d95e32
Alice Pote <alice.writes.wrongs@gmail.com>
2016-04-16 10:32:57 -0400

    refactoring to single page app

---
+++ app/react/containers/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class App extends React.Component {
+  render () {
+    return (
+      <div id="foo">
+        hey
+      </div>
+    );
+  }
+}
+
+export default App;
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'static_pages#home'
 
-  # static pages
+  # urls we want react-router to handle
   get '/' => 'static_pages#home'
-  get '/home' => 'static_pages#home'
-  get '/groceries' => 'static_pages#grocery_list'
+  get '/calendar' => 'static_pages#home'
+  get '/groceries' => 'static_pages#home'
 
   # API
   get '/grocery_items/unpurchased' => 'grocery_items#unpurchased'
